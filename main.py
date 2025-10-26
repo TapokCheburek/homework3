c5ed913467e80635cf0f48311ed7d57c19ae69c3
VolkovaDN <Volkova.DN@in4security.com>
2025-10-26 20:12:43 +0300

    last_commit

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from product import Product, Order, Store
 
-if __name__ == '__main__':\
+if __name__ == '__main__':
+    ''''Будем считать, что у каждого продукта есть склад, продукт может быть
+     доступен в нескольких магазинах, но его кол-во везде будет одинаковым'''
 
     product1 = Product("Ноутбук", 1000, 5)
     product2 = Product("Смартфон", 500, 10)
-    order1 = Order()
-    # order1.add_product(product1, 3)
-    # order1.add_product(product1, 4)
-    # sum = order1.calculate_total()
-    # print(sum)
+
     store1 = Store()
     store1.add_product(product1)
     store1.add_product(product2)
     store1.list_products()
+
+    ''''Будем считать, что мы добавляем в заказ товары, но
+     они не удаляются со склада пока не "оплатишь"'''
+    order1 = Order()
+    order1.add_product(product1, 6)
+    order1.add_product(product2, 4)
+    total = order1.calculate_total()
+    print(f"Общая стоимость заказа {total}")
+
+    order1.remove_product(product1, 5)
+    total = order1.calculate_total()
+    print(f"Общая стоимость заказа {total}")
+
+    '''' Будем считаем, что заказ "оплатили" -> товары списались со клада;
+         где-то в другом месте поменялся статус заказа;
+         после оплаты можно применять метод return_product'''
+    order1.pay()
+    store1.list_products()
+
+    order1.return_product(product2, 3)
+    total = order1.calculate_total()
+    print(f"Общая стоимость заказа {total}")
+    store1.list_products()
+
+
+
 
--- product.py
+++ product.py
@@ -11,11 +11,16 @@ class Product():
             print("Количество товара на складе не может получиться отрицательным!\n"
                   "Не получилось взять товар со склада")
 
+
 class Order():
     def __init__(self):
         self.products  = {}
 
     def add_product(self, product, quantity = 0):
+        if product.stock < quantity:
+            print(f"Количество продуктов {product.name} на складе меньше {quantity}\n"
+                  f"Добавлено в заказ {product.stock} {product.name} ")
+            quantity = product.stock
         if product in self.products:
             self.products[product] += quantity
         else:
@@ -24,11 +29,34 @@ class Order():
     def calculate_total(self):
         total = 0
         for product in self.products:
+            print( f"{product.name}  - {self.products[product]} шт.  {product.price} р.")
             total += self.products[product] * product.price
-            return total
+
+        return total
+
+    def pay(self):
+        for product in self.products:
+            product.update_stock(-self.products[product])
+
+    def remove_product(self, product, quantity):
+        if self.products[product] <= quantity:
+            self.products.pop(product)
+        else:
+            self.products[product] -= quantity
+
+    def return_product(self, product, quantity):
+        if self.products[product] <= quantity:
+            quantity = self.products[product]
+            self.products.pop(product)
+        else:
+            self.products[product] -= quantity
+        product.update_stock(quantity)
+
 
 class Store():
-    def __init__(self, products = []):
+    def __init__(self, products: list[Product] = None):
+        if products is None:
+            products = []
         self.products = products
 
     def add_product(self, product):
@@ -39,6 +67,9 @@ class Store():
             print(f"Товар {product.name} добавлен в магазин")
 
     def list_products(self):
+        print("Все товары в магазине:")
         for product in self.products:
             print(f"Товар: {product.name}, цена: {product.price}, кол-во: {product.stock}")
 
+    def create_order(self):
+        return Order()
